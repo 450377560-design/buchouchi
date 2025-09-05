d0631f0a8ccd1685176a8f52bd752a1a60a0f3f1
450377560-design <450377560@qq.com>
2025-09-05 17:38:15 +0800

    Update cuisine.dart

--- lib/models/cuisine.dart
+++ lib/models/cuisine.dart
@@ -1,41 +1,60 @@
+/// 八大菜系 + 自定义
 enum Cuisine {
-  chuancai,  // 川
-  yuecai,    // 粤
-  sucai,     // 苏
-  zhecai,    // 浙
-  mincai,    // 闽
-  xiangcai,  // 湘
-  huicai,    // 徽
-  lucai,     // 鲁
-  custom,    // 自定义专区
+  chuancai, // 川
+  yuecai,   // 粤
+  sucai,    // 苏
+  zhecai,   // 浙
+  mincai,   // 闽
+  xiangcai, // 湘
+  huicai,   // 徽
+  lucai,    // 鲁
+  custom,   // 自定义
 }
 
-extension CuisineX on Cuisine {
-  String get zh {
-    switch (this) {
-      case Cuisine.chuancai: return '川菜';
-      case Cuisine.yuecai:   return '粤菜';
-      case Cuisine.sucai:    return '苏菜';
-      case Cuisine.zhecai:   return '浙菜';
-      case Cuisine.mincai:   return '闽菜';
-      case Cuisine.xiangcai: return '湘菜';
-      case Cuisine.huicai:   return '徽菜';
-      case Cuisine.lucai:    return '鲁菜';
-      case Cuisine.custom:   return '自定义菜谱';
+/// 提供中文名、列表与反序列化工具
+class CuisineX {
+  /// 八大菜系列表（不含自定义）
+  static const List<Cuisine> eight = [
+    Cuisine.chuancai,
+    Cuisine.yuecai,
+    Cuisine.sucai,
+    Cuisine.zhecai,
+    Cuisine.mincai,
+    Cuisine.xiangcai,
+    Cuisine.huicai,
+    Cuisine.lucai,
+  ];
+
+  /// 通过存储键（enum 名字）还原为枚举
+  static Cuisine fromKey(String key) {
+    switch (key) {
+      case 'chuancai':
+        return Cuisine.chuancai;
+      case 'yuecai':
+        return Cuisine.yuecai;
+      case 'sucai':
+        return Cuisine.sucai;
+      case 'zhecai':
+        return Cuisine.zhecai;
+      case 'mincai':
+        return Cuisine.mincai;
+      case 'xiangcai':
+        return Cuisine.xiangcai;
+      case 'huicai':
+        return Cuisine.huicai;
+      case 'lucai':
+        return Cuisine.lucai;
+      case 'custom':
+      default:
+        return Cuisine.custom;
     }
   }
 
-  String get key => toString().split('.').last;
-
-  static Cuisine fromKey(String key) {
-    return Cuisine.values.firstWhere(
-      (c) => c.key == key,
-      orElse: () => Cuisine.custom,
-    );
-  }
-
-  static List<Cuisine> get eight => [
-    Cuisine.chuancai, Cuisine.yuecai, Cuisine.sucai, Cuisine.zhecai,
-    Cuisine.mincai, Cuisine.xiangcai, Cuisine.huicai, Cuisine.lucai,
-  ];
-}
+  /// 枚举 -> 中文名
+  static String zh(Cuisine c) {
+    switch (c) {
+      case Cuisine.chuancai:
+        return '川菜';
+      case Cuisine.yuecai:
+        return '粤菜';
+      case Cuisine.sucai:
